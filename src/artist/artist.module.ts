49f14bf4e3e6e77fe2219d3d3308512a3962553f
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-02-14 16:22:10 +0300

    feat: rewrite artist service to work with database

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,5 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { FavsModule } from 'src/favs/favs.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { TrackModule } from 'src/track/track.module';
 import { ArtistController } from './artist.controller';
 import { ArtistService } from './artist.service';
@@ -7,7 +8,7 @@ import { ArtistService } from './artist.service';
 @Module({
   controllers: [ArtistController],
   providers: [ArtistService],
-  imports: [TrackModule, forwardRef(() => FavsModule)],
+  imports: [TrackModule, forwardRef(() => FavsModule), PrismaModule],
   exports: [ArtistService],
 })
 export class ArtistModule {}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,53 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
-import { Artist } from './model/artist.model';
-import { v4 as uuidv4 } from 'uuid';
 import { TrackService } from 'src/track/track.service';
 import { FavsService } from 'src/favs/favs.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ArtistService {
-  artistsMock: Artist[] = [];
   constructor(
+    private readonly prisma: PrismaService,
     private readonly trackService: TrackService,
     private readonly favsService: FavsService,
   ) {}
   async getAllArtists() {
-    return this.artistsMock;
+    return this.prisma.artist.findMany();
   }
 
   async getArtistById(id: string) {
-    return this.artistsMock.find((artist: Artist) => artist.id === id);
+    return this.prisma.artist.findUnique({
+      where: { id: id },
+    });
   }
 
   async createArtist(dto: CreateArtistDto) {
     const { name, grammy } = dto;
-    const newArtist: Artist = {
-      id: uuidv4(),
-      name,
-      grammy,
-    };
-    this.artistsMock.push(newArtist);
-    return newArtist;
+    return await this.prisma.artist.create({
+      data: {
+        name,
+        grammy,
+      },
+    });
   }
 
   async updateArtist(id: string, dto: CreateArtistDto) {
     const { name, grammy } = dto;
-    const updatedArtist = {
-      id,
-      name,
-      grammy,
-    };
-    this.artistsMock = this.artistsMock.map((artist: Artist) =>
-      artist.id === id ? { ...updatedArtist } : artist,
-    );
-    return updatedArtist;
+    return await this.prisma.artist.update({
+      where: { id: id },
+      data: {
+        name,
+        grammy,
+      },
+    });
   }
 
   async deleteArtist(id: string) {
-    this.artistsMock = this.artistsMock.filter(
-      (artist: Artist) => artist.id !== id,
-    );
+    await this.prisma.artist.delete({
+      where: { id: id },
+    });
     this.trackService.artistWasDeleted(id);
     this.favsService.deleteArtistFromFavs(id);
   }
